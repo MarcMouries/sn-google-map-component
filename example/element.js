4c80cb216a90a809a09e85475bdeceb0093e1c7f
Marc Mouries <marc@mouries.net>
2022-12-12 08:41:11 -0500

    + sample data

--- example/element.js
+++ example/element.js
@@ -4,5 +4,5 @@ const el = document.createElement('DIV');
 document.body.appendChild(el);
 
 el.innerHTML = `
-<x-snc-googlemap data="[{"my-data: "dummy"}]]"></x-snc-googlemap>
+<x-snc-googlemap data=[{table: "task", sys_id: "ac26c34c1be37010b93654a2604bcbd5", lat: 38.913014, long: -77.224186, image: "https://pbs.twimg.com/profile_images/1483341411911041025/hY5z-EDB_400x400.jpg"}]></x-snc-googlemap>
 `;

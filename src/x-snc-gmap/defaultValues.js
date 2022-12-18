f2ba4128aff4ba886e31332fea27a4006f6a039b
Marc Mouries <marc@mouries.net>
2022-12-17 22:38:44 -0500

    new

---
+++ src/x-snc-gmap/defaultValues.js
@@ -0,0 +1,19 @@
+export const DEFAULT_VALUES = [
+    /* 8045 Leesburg Pike, Vienna, VA 22182, United States of America */
+    {
+        table: "task",
+        sys_id: "ac26c34c1be37010b93654a2604bcbd5",
+        lat: 38.913014,
+        long: -77.224186,
+        image: 'https://www.servicenow.com/content/dam/servicenow-assets/public/en-us/images/company-library/icon/icon-locations.svg',
+    }
+    ,
+    /* New York, New York, United States of America */
+    {
+        table: "sys_user",
+        sys_id: "5137153cc611227c000bbd1bd8cd2005",
+        lat: 40.7896239,
+        long: -73.9598939,
+        image: 'https://www.servicenow.com/content/dam/servicenow-assets/public/en-us/images/company-library/icon/icon-locations.svg'
+    }
+]

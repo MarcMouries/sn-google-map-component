c774b878bc2d607914d9371b4554483e17dc6c65
Marc Mouries <marc@mouries.net>
2022-12-17 22:36:36 -0500

    styling

--- example/element.js
+++ example/element.js
@@ -3,7 +3,12 @@ import '../src/x-snc-gmap';
 const el = document.createElement('DIV');
 document.body.appendChild(el);
 
-el.innerHTML = `<div id="example-container" style='width: 500px; height: 500px;'><x-snc-gmap></x-snc-gmap></div>`;
+el.innerHTML =`
+<div id="example-container"
+       style='width: 500px; height: 500px;'>
+       <x-snc-gmap>
+       </x-snc-gmap>
+</div>`;
 
 /*
 data=[{table: \"task\", sys_id: \"ac26c34c1be37010b93654a2604bcbd5\", lat: 38.913014, long: -77.224186, image: \"https://pbs.twimg.com/profile_images/1483341411911041025/hY5z-EDB_400x400.jpg\"}]

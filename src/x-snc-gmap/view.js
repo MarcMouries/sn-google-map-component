b2a0d43b354b754fc71423257a57877dcba16cc5
Marc Mouries <marc@mouries.net>
2023-01-24 07:56:52 -0500

    use translate

--- src/x-snc-gmap/view.js
+++ src/x-snc-gmap/view.js
@@ -1,15 +1,16 @@
-import { Fragment } from '@seismic/snabbdom-renderer';
+import { Fragment } from "@seismic/snabbdom-renderer";
 import { stateConstants } from "./constants";
-import {t} from 'sn-translate';
+import { customActions } from "./constants"
+import {translate} from './translate';
 
-export default (state) => {
-    console.log("Map Component: state ", state);
-    const { mapElementRef, autoCompleteRef } = state;
-    const { properties } = state;
-    const minHeight = "100%";
-    const height = "100%";
+export default (state, { updateState, dispatch }) => {
+  console.log("Map Component: state ", state);
+  const { mapElementRef, autoCompleteRef } = state;
+  const { properties } = state;
+  const minHeight = "100%";
+  const height = "100%";
 
-    /* 
+  /* 
     if (state.isLoading) {
         return (
             <Fragment>
@@ -21,19 +22,22 @@ export default (state) => {
     }
     else {
         */
-    return (
-        <Fragment>
-            <div className="map-container">
-                <div className="input-container">
-                    <label>Address</label>
-                    <input type="text" id="address-input" className="address-input" placeholder={t("Enter an address")} ref={autoCompleteRef}/>
-                </div>
-                <div id="map">
-                    <div style={{ height: height, "min-height": minHeight }} ref={mapElementRef}>
-                    </div>
-                </div>
-            </div>
-        </Fragment>
-    );
-    // }
+
+  return (
+    <Fragment>
+      <div className="map-container">
+        <button type="button" on-click={(event) => dispatch(customActions.ACTION_ON_CLICK, { table: "example" })}>
+          CLICK
+        </button>
+        <div className="input-container">
+          <label>Address</label>
+          <input type="text" id="address-input" className="address-input" placeholder={translate("Enter an address")} ref={autoCompleteRef} />
+        </div>
+        <div id="map">
+          <div style={{ height: height, "min-height": minHeight }} ref={mapElementRef}></div>
+        </div>
+      </div>
+    </Fragment>
+  );
+  // }
 };

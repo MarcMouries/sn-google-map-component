70d1b7db2e87ea6b29b9e6e8dc04ee24f9007fce
Marc Mouries <marc@mouries.net>
2023-01-24 07:57:25 -0500

    fix retrieval of translations for undefined msg

--- src/x-snc-gmap/translate.js
+++ src/x-snc-gmap/translate.js
@@ -11,6 +11,9 @@ const newLocale = "en";
 export  function translate (message_key, language) {
 
     console.log ("  - translate : translations = " +  JSON.stringify(translations));
+
+    const message_translations  = translations[message_key];
+    if (! message_translations)  return `translation for ${message_key} is undefined`;
     const message = translations[message_key][language];
     console.log ("  - retrieved message: " +  message);
     const result = message ? message : translations[message_key]["en"];

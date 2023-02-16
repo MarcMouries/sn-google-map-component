6b6ff741021e5dc8fae27f0fdc47066953b03ff1
MarcMouries <marc@mouries.net>
2023-02-16 18:11:40 -0500

    clean up

--- src/x-snc-gmap/translate.js
+++ src/x-snc-gmap/translate.js
@@ -1,22 +1,17 @@
-
+/**
+ * Simple translation class that allows to bundle the translations with the component
+ */
 import translations from './translations';
-
-//let translations = {};
 
 console.log (" == loading the translate.js ==");
 console.log ("  - translations = ", translations);
 
-const newLocale = "en";
-
 export  function translate (message_key, language) {
-
-    console.log ("  - translate : translations = " +  JSON.stringify(translations));
-
     const message_translations  = translations[message_key];
     if (! message_translations)  return `translation for ${message_key} is undefined`;
     const message = translations[message_key][language];
     console.log ("  - retrieved message: " +  message);
     const result = message ? message : translations[message_key]["en"];
-    console.log(`translated: ${message_key} to ${result}`);
+    console.log(`translated: ${message_key} ⮕ ${result}`);
     return result;
 }

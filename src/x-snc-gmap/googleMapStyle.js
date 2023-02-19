abae1b46da5b907bc4423c3e134660d9fdf603d3
MarcMouries <marc@mouries.net>
2023-02-18 23:21:17 -0500

    new

---
+++ src/x-snc-gmap/googleMapStyle.js
@@ -0,0 +1,160 @@
+export const Road_Atlas = 
+[
+    {
+      featureType: "landscape.man_made",
+      elementType: "all",
+      stylers: [
+        { lightness: -46 },
+        { saturation: 100 },
+        { hue: "#ffee00" },
+        { gamma: 0.94 }
+      ]
+    },{
+      featureType: "road",
+      elementType: "geometry",
+      stylers: [
+        { hue: "#2a00ff" },
+        { visibility: "simplified" },
+        { lightness: -10 },
+        { saturation: -60 }
+      ]
+    },{
+      featureType: "road.arterial",
+      elementType: "geometry",
+      stylers: [
+        { visibility: "simplified" },
+        { lightness: 20 },
+        { hue: "#ff0066" },
+        { saturation: 20 }
+      ]
+    },{
+      featureType: "road",
+      elementType: "labels",
+      stylers: [
+        { visibility: "simplified" }
+      ]
+    },{
+      featureType: "water",
+      elementType: "all",
+      stylers: [
+        { lightness: 40 }
+      ]
+    },{
+      featureType: "administrative.locality",
+      elementType: "all",
+      stylers: [
+        { lightness: 20 }
+      ]
+    }
+  ]
+
+  export const Yahoo_Map = 
+  [
+    {
+      featureType: "all",
+      elementType: "all",
+      stylers: [
+        { saturation: 40 }
+      ]
+    },{
+      featureType: "road.highway",
+      elementType: "geometry",
+      stylers: [
+        { hue: "#ff1100" },
+        { gamma: 1 },
+        { lightness: -30 },
+        { saturation: -20 }
+      ]
+    },{
+      featureType: "administrative.locality",
+      elementType: "all",
+      stylers: [
+        { hue: "#fff700" },
+        { saturation: 100 },
+        { lightness: 0 },
+        { visibility: "on" },
+        { gamma: 1 }
+      ]
+    },{
+      featureType: "road.arterial",
+      elementType: "geometry",
+      stylers: [
+        { hue: "#ff9100" },
+        { lightness: -20 }
+      ]
+    },{
+      featureType: "road",
+      elementType: "labels",
+      stylers: [
+        { saturation: -10 },
+        { visibility: "simplified" }
+      ]
+    },{
+      featureType: "poi.park",
+      elementType: "all",
+      stylers: [
+        { saturation: -20 }
+      ]
+    }
+  ]
+
+  export const MapQuest = 
+  [
+    {
+      featureType: "road.highway",
+      elementType: "geometry",
+      stylers: [
+        { hue: "#0033ff" },
+        { lightness: 10 },
+        { saturation: -50 }
+      ]
+    },{
+      featureType: "road.arterial",
+      elementType: "all",
+      stylers: [
+        { hue: "#ffc300" }
+      ]
+    },{
+      featureType: "road.highway",
+      elementType: "labels",
+      stylers: [
+        { visibility: "simplified" },
+        { gamma: 0.6 },
+        { lightness: 0 }
+      ]
+    },{
+      featureType: "transit.station",
+      elementType: "all",
+      stylers: [
+        { visibility: "on" },
+        { gamma: 0.01 }
+      ]
+    },{
+      featureType: "landscape",
+      elementType: "all",
+      stylers: [
+        { hue: "#88ff00" },
+        { lightness: -1 },
+        { saturation: -10 }
+      ]
+    },{
+      featureType: "water",
+      elementType: "geometry",
+      stylers: [
+        { saturation: 30 },
+        { lightness: 50 }
+      ]
+    },{
+      featureType: "poi",
+      elementType: "all",
+      stylers: [
+        { visibility: "on" }
+      ]
+    },{
+      featureType: "landscape",
+      elementType: "labels",
+      stylers: [
+        { visibility: "on" }
+      ]
+    }
+  ]

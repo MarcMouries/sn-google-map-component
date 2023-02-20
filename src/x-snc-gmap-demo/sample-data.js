412a748d17007ca658be7a74ec45e7ec68d8b7b0
MarcMouries <marc@mouries.net>
2023-02-20 15:48:13 -0500

    new

---
+++ src/x-snc-gmap-demo/sample-data.js
@@ -0,0 +1,29 @@
+export const AIRPORTS = [
+    {
+      iata_code: "DCA",
+      name: "Ronald Reagan Washington National Airport",
+      city: "Arlington",
+      state: "Virginia",
+      country: "United States",
+      lat: 38.852083,
+      lng: -77.037722,
+    },
+    {
+      iata_code: "IAD",
+      name: "Washington Dulles International Airport",
+      city: "Dulles",
+      state: "Virginia",
+      country: "United States",
+      lat: 38.95363651373208,
+      lng: -77.44828266850212,
+    },
+    {
+      iata_code: "BWI",
+      name: "Baltimore/Washington International Thurgood Marshal Airport",
+      city: "Baltimore",
+      state: "Maryland",
+      country: "United States",
+      lat: 39.175361,
+      lng: -76.668333,
+    },
+  ];

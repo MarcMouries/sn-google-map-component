79be6bfbf8f9329bc4ab5d3d70ed87432ca12e54
MarcMouries <marc@mouries.net>
2023-02-20 11:24:08 -0500

    + city

--- src/x-snc-gmap/defaultValues.js
+++ src/x-snc-gmap/defaultValues.js
@@ -1,48 +1,48 @@
-
 export const AIRPORTS = [
-    {
-      "name": "Ronald Reagan Washington Natl",
-      "city": "Washington",
-      "country": "United States",
-      "iata_code": "DCA",
-      "lat": 38.852083,
-      "lng": -77.037722
-    },
-    {
-      "name": "Washington Dulles Intl",
-      "city": "Washington",
-      "country": "United States",
-      "iata_code": "IAD",
-      "lat": 38.95363651373208,
-      "lng": -77.44828266850212
-    },
-    {
-      "name": "Baltimore Washington Intl",
-      "city": "Baltimore",
-      "country": "United States",
-      "iata_code": "BWI",
-      "lat": 39.175361,
-      "lng": -76.668333
-    }
-  ]
-
+  {
+    iata_code: "DCA",
+    name: "Ronald Reagan Washington National Airport",
+    city: "Arlington",
+    state: "Virginia",
+    country: "United States",
+    lat: 38.852083,
+    lng: -77.037722,
+  },
+  {
+    iata_code: "IAD",
+    name: "Washington Dulles International Airport",
+    city: "Dulles",
+    state: "Virginia",
+    country: "United States",
+    lat: 38.95363651373208,
+    lng: -77.44828266850212,
+  },
+  {
+    iata_code: "BWI",
+    name: "Baltimore/Washington International Thurgood Marshal Airport",
+    city: "Baltimore",
+    state: "Maryland",
+    country: "United States",
+    lat: 39.175361,
+    lng: -76.668333,
+  },
+];
 
 export const DEFAULT_VALUES = [
-    /* David Loo's location */
-    {
-        table: "sys_user",
-        sys_id: "5137153cc611227c000bbd1bd8cd2007",
-        lat: 32.990093,
-        lng: -117.273023,
-        image: 'https://www.servicenow.com/content/dam/servicenow-assets/public/en-us/images/company-library/icon/icon-locations.svg',
-    }
-    ,
-    /* Fred Luddy's location */
-    {
-        table: "sys_user",
-        sys_id: "5137153cc611227c000bbd1bd8cd2005",
-        lat: 32.582376,
-        lng: -117.034507,
-        image: 'https://www.servicenow.com/content/dam/servicenow-assets/images/meganav/servicenow-header-logo.svg'
-    }
-]
+  /* David Loo's location */
+  {
+    table: "sys_user",
+    sys_id: "5137153cc611227c000bbd1bd8cd2007",
+    lat: 32.990093,
+    lng: -117.273023,
+    image: "https://www.servicenow.com/content/dam/servicenow-assets/public/en-us/images/company-library/icon/icon-locations.svg",
+  },
+  /* Fred Luddy's location */
+  {
+    table: "sys_user",
+    sys_id: "5137153cc611227c000bbd1bd8cd2005",
+    lat: 32.582376,
+    lng: -117.034507,
+    image: "https://www.servicenow.com/content/dam/servicenow-assets/images/meganav/servicenow-header-logo.svg",
+  },
+];

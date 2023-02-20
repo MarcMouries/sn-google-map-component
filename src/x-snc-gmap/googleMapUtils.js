a1c86ee86add01a18ec244a380e61e486604a0ef
MarcMouries <marc@mouries.net>
2023-02-20 11:02:06 -0500

    + formatAddress

--- src/x-snc-gmap/googleMapUtils.js
+++ src/x-snc-gmap/googleMapUtils.js
@@ -6,7 +6,7 @@
 
 import { COLOR } from "./constants";
 
- const defaultCircleOptions = {
+const defaultCircleOptions = {
   strokeColor: COLOR.CIRCLE,
   strokeOpacity: 0.6,
   strokeWeight: 1,
@@ -29,7 +29,6 @@ export function createCircle(map, center, radius, options) {
   circle.setRadius(radius);
   return circle;
 }
-
 
 /* Compute the position of the marker at the top of the circle */
 export function computeMarkerPosition(circle, position) {
@@ -74,7 +73,7 @@ export function getCircleRadiusDescription(circle) {
   return circleRadiusDesc;
 }
 
- /**
+/**
    * Retrieve place details based on the place description ("ServiceNow, leesburg pike")
    It returns a Promise that resolves with the place details if the request is successful,
    or rejects with an error status if the request fails.
@@ -104,32 +103,94 @@ export function getPlaceDetails(placeDescription, googleMap) {
 }
 
 export function createInfoWindowFromObject(title, obj) {
-    var content = document.createElement('div');
+  console.log("🌎 createInfoWindowFromObject createInfoWindow createInfoWindow", place);
 
-    // Create a header with the value of the title parameter
-    var header = document.createElement('h3');
-    header.textContent = title;
-    content.appendChild(header);
+  var content = document.createElement("div");
 
-    // Iterate over each property of the object and add it to the content
-    for (var prop in obj) {
-      if (obj.hasOwnProperty(prop) && prop !== 'name') {
-        var element = document.createElement('p');
-        element.innerHTML = '<strong>' + prop + ': </strong>' + obj[prop];
-        content.appendChild(element);
-      }
+  // Create a header with the value of the title parameter
+  var header = document.createElement("h3");
+  header.textContent = title;
+  content.appendChild(header);
+
+  // Iterate over each property of the object and add it to the content
+  for (var prop in obj) {
+    if (obj.hasOwnProperty(prop) && prop !== "name") {
+      var element = document.createElement("p");
+      element.innerHTML = "<strong>" + prop + ": </strong>" + obj[prop];
+      content.appendChild(element);
     }
-
-    // Create a string with the HTML content for the InfoWindow
-    var contentString = content.outerHTML;
-
-    // Create a new InfoWindow with the content string
-    var infowindow = new google.maps.InfoWindow({
-      content: contentString
-    });
-    
-    return infowindow;
   }
-  
 
+  // Create a string with the HTML content for the InfoWindow
+  var contentString = content.outerHTML;
 
+  // Create a new InfoWindow with the content string
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString,
+  });
+
+  return infowindow;
+}
+
+/**
+ * Construct an address based on the google address_components that
+ * follows this format: 8045 Leesburg Pike STE 300, Vienna, VA 22182, USA
+ * @param {*} address_components 
+ * @returns well formatted address
+ */
+function formatAddress(address_components) {
+  let addressLine1 = "";
+  let cityStateZip = "";
+  let subpremise = "";
+
+  for (let component of address_components) {
+    if (component.types.includes("street_number")) {
+      addressLine1 += component.long_name + " ";
+    }
+    if (component.types.includes("route")) {
+      addressLine1 += component.long_name + " ";
+    }
+    if (component.types.includes("subpremise")) {
+      subpremise = component.long_name;
+    }
+    if (component.types.includes("locality")) {
+      cityStateZip += component.long_name + ", ";
+    }
+    if (component.types.includes("administrative_area_level_1")) {
+      cityStateZip += component.short_name + " ";
+    }
+    if (component.types.includes("postal_code")) {
+      cityStateZip += component.long_name + ", ";
+    }
+  }
+
+  // Remove trailing comma and space from cityStateZip
+  //cityStateZip = cityStateZip.slice(0, -2);
+
+  // Combine address lines and return
+  return addressLine1 + subpremise + ", " + cityStateZip;
+}
+
+export function createInfoWindow(place) {
+  console.log("🌎 createInfoWindow createInfoWindow createInfoWindow", place);
+
+  const content = document.createElement("div");
+  const name = document.createElement("div");
+  const addressDiv = document.createElement("div");
+  // name.textContent = place.name;
+  name.innerHTML = `<b>${place.name}</b>`;
+
+  const { address_components } = place;
+  // Create a formatted address string using the address_components variable
+  // using the format: 8045 Leesburg Pike STE 300, Vienna, VA 22182, USA
+  //const address = `${address_components[0].long_name} ${address_components[1].long_name}, ${address_components[2].long_name}, ${address_components[4].short_name} ${address_components[6].long_name}, ${address_components[5].long_name}`;
+  const address = formatAddress(place.address_components);
+  //address.textContent = place.formatted_address;
+  //addressDiv.innerHTML = place.adr_address;
+  addressDiv.innerHTML = address;
+
+  content.appendChild(name);
+  content.appendChild(addressDiv);
+  const infowindow = new google.maps.InfoWindow({ content: content });
+  return infowindow;
+}

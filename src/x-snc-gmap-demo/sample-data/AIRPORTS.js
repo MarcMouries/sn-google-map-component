93ac889ade9fb9ec15e938d0d9aa00304b0329e8
MarcMouries <Marc@Mouries.net>
2023-06-28 15:40:52 -0400

    new

---
+++ src/x-snc-gmap-demo/sample-data/AIRPORTS.js
@@ -0,0 +1,37 @@
+export const AIRPORTS = [
+  {
+    iata_code: "DCA",
+    name: "Ronald Reagan Washington National Airport",
+    city: "Arlington",
+    state: "Virginia",
+    country: "United States",
+    "country_2": {
+      "code": "US",
+      "name": "United States"
+    },
+    position: { lat: 38.852083, lng: -77.037722 }
+  },
+  {
+    iata_code: "IAD",
+    name: "Washington Dulles International Airport",
+    city: "Dulles",
+    state: "Virginia",
+    country: "United States",
+    country_2 : {
+      "code": "US",
+      "name": "United States"
+    },
+    position: { lat: 38.95363651373208, lng: -77.44828266850212 }
+  },
+  {
+    iata_code: "BWI",
+    name: "Baltimore/Washington International Thurgood Marshall Airport",
+    city: "Baltimore",
+    state: "Maryland",
+    country: "United States",
+    "country_2": {
+      "code": "US",
+      "name": "United States"
+    }, position: { lat: 39.175361, lng: -76.668333 }
+  },
+];
---
+++ src/x-snc-gmap-demo/sample-data/MONEY_ORDERS.js
@@ -0,0 +1,339 @@
+export const MONEY_ORDERS = 
+[
+    {
+        "serialNumber": "88002206235",
+        "position": {
+            "lat": 38.9293,
+            "lng": -77.237
+        },
+        "postOffice": {
+            "location": "1437410",
+            "financeNumberUnitId": "1122334455",
+            "name": "WEST MCLEAN",
+            "address": {
+                "street": "1544 Spring Hill Rd",
+                "city": "MC LEAN",
+                "state": "VA",
+                "zipCode": "22102",
+                "country": "United States"
+            }
+        },
+        "u8105A": {
+            "key": null,
+            "entryDate": "",
+            "ctrDate": ""
+        },
+        "transaction": {
+            "date": "2020-01-11",
+            "time": "0830",
+            "amount": "600",
+            "clerk": "002",
+            "debit": "N"
+        },
+        "sender": {
+            "name": "One Cashew",
+            "address": {
+                "street": "8051 Leesburg Pike",
+                "city": "Vienna",
+                "state": "VA",
+                "zipCode": "22182"
+            }
+        },
+        "recipient": {
+            "name": "Two Cashew",
+            "address": {
+                "street": "8133 Leesburg Pike",
+                "city": "Vienna",
+                "state": "VA",
+                "zipCode": "22182"
+            }
+        },
+        "purchaser": {
+            "name": "null null",
+            "address": {
+                "street": null,
+                "city": null,
+                "state": null,
+                "zipCode": null
+            }
+        },
+        "image": "d72d34c3c363291052837e3c05013129",
+        "image_ref": "/d72d34c3c363291052837e3c05013129.iix",
+        "name": "Money Order #88002206235",
+        "_date_time": "2020-01-11 @ 0830",
+        "_amount": "$600",
+        "_sender": "One Cashew",
+        "_recipient": "Two Cashew",
+        "_postOffice": "WEST MCLEAN",
+        "_location": "MC LEAN, VA"
+    },
+    {
+        "serialNumber": "88002206236",
+        "position": {
+            "lat": 38.9357,
+            "lng": -77.18
+        },
+        "postOffice": {
+            "location": "1372446",
+            "financeNumberUnitId": "1212343455",
+            "name": "MC LEAN",
+            "address": {
+                "street": "6841 ELM ST",
+                "city": "MC LEAN",
+                "state": "VA",
+                "zipCode": "22101",
+                "country": "United States"
+            }
+        },
+        "u8105A": {
+            "key": null,
+            "entryDate": "",
+            "ctrDate": ""
+        },
+        "transaction": {
+            "date": "2020-01-12",
+            "time": "0900",
+            "amount": "350",
+            "clerk": "004",
+            "debit": "N"
+        },
+        "sender": {
+            "name": "One Cashew",
+            "address": {
+                "street": "8051 Leesburg Pike",
+                "city": "Vienna",
+                "state": "VA",
+                "zipCode": "22182"
+            }
+        },
+        "recipient": {
+            "name": "Three Cashew",
+            "address": {
+                "street": "6730 Curran St",
+                "city": "McLean",
+                "state": "VA",
+                "zipCode": "22101"
+            }
+        },
+        "purchaser": {
+            "name": "null null",
+            "address": {
+                "street": null,
+                "city": null,
+                "state": null,
+                "zipCode": null
+            }
+        },
+        "image": "4b1db0c3c363291052837e3c050131f8",
+        "image_ref": "/4b1db0c3c363291052837e3c050131f8.iix",
+        "name": "Money Order #88002206236",
+        "_date_time": "2020-01-12 @ 0900",
+        "_amount": "$350",
+        "_sender": "One Cashew",
+        "_recipient": "Three Cashew",
+        "_postOffice": "MC LEAN",
+        "_location": "MC LEAN, VA"
+    },
+    {
+        "serialNumber": "88002206234",
+        "position": {
+            "lat": 39.175361,
+            "lng": -76.668333
+        },
+        "postOffice": {
+            "location": null,
+            "financeNumberUnitId": "2233445566",
+            "name": "BWI",
+            "address": {
+                "street": null,
+                "city": "BALTIMORE",
+                "state": "MD",
+                "zipCode": "21240",
+                "country": null
+            }
+        },
+        "u8105A": {
+            "key": null,
+            "entryDate": "2020-01-12",
+            "ctrDate": "2020-01-12"
+        },
+        "transaction": {
+            "date": "2020-01-12",
+            "time": "0900",
+            "amount": "800",
+            "clerk": "003",
+            "debit": "N"
+        },
+        "sender": {
+            "name": "Two Cashew",
+            "address": {
+                "street": null,
+                "city": "Baltimore",
+                "state": "MD",
+                "zipCode": null
+            }
+        },
+        "recipient": {
+            "name": "One Cashew",
+            "address": {
+                "street": null,
+                "city": "Mc Lean",
+                "state": "VA",
+                "zipCode": null
+            }
+        },
+        "purchaser": {
+            "name": "null null",
+            "address": {
+                "street": null,
+                "city": null,
+                "state": null,
+                "zipCode": null
+            }
+        },
+        "image": null,
+        "image_ref": null,
+        "name": "Money Order #88002206234",
+        "_date_time": "2020-01-12 @ 0900",
+        "_amount": "$800",
+        "_sender": "Two Cashew",
+        "_recipient": "One Cashew",
+        "_postOffice": "BWI",
+        "_location": "BALTIMORE, MD"
+    },
+    {
+        "serialNumber": "88002206237",
+        "position": {
+            "lat": 38.9975,
+            "lng": -77.292
+        },
+        "postOffice": {
+            "location": "1437309",
+            "financeNumberUnitId": "3213213211",
+            "name": "GREAT FALLS",
+            "address": {
+                "street": "10001 GEORGETOWN PIKE",
+                "city": "GREAT FALLS",
+                "state": "VA",
+                "zipCode": "22066",
+                "country": "United States"
+            }
+        },
+        "u8105A": {
+            "key": null,
+            "entryDate": "",
+            "ctrDate": ""
+        },
+        "transaction": {
+            "date": "2020-01-12",
+            "time": "0930",
+            "amount": "700",
+            "clerk": "001",
+            "debit": "N"
+        },
+        "sender": {
+            "name": "Two Cashew",
+            "address": {
+                "street": "8133 Leesburg Pike",
+                "city": "Vienna",
+                "state": "VA",
+                "zipCode": "22182"
+            }
+        },
+        "recipient": {
+            "name": "One Cashew",
+            "address": {
+                "street": "8051 Leesburg Pike",
+                "city": "Vienna",
+                "state": "VA",
+                "zipCode": "22182"
+            }
+        },
+        "purchaser": {
+            "name": "null null",
+            "address": {
+                "street": null,
+                "city": null,
+                "state": null,
+                "zipCode": null
+            }
+        },
+        "image": "a5adfcc3c363291052837e3c05013126",
+        "image_ref": "/a5adfcc3c363291052837e3c05013126.iix",
+        "name": "Money Order #88002206237",
+        "_date_time": "2020-01-12 @ 0930",
+        "_amount": "$700",
+        "_sender": "Two Cashew",
+        "_recipient": "One Cashew",
+        "_postOffice": "GREAT FALLS",
+        "_location": "GREAT FALLS, VA"
+    },
+    {
+        "serialNumber": "89002206237",
+        "position": {
+            "lat": 38.9475,
+            "lng": -77.332
+        },
+        "postOffice": {
+            "location": "1437366",
+            "financeNumberUnitId": "4213213211",
+            "name": "RESTON",
+            "address": {
+                "street": "11110 Sunset Hills Rd",
+                "city": "RESTON",
+                "state": "VA",
+                "zipCode": "20190",
+                "country": "United States"
+            }
+        },
+        "u8105A": {
+            "key": null,
+            "entryDate": "",
+            "ctrDate": ""
+        },
+        "transaction": {
+            "date": "2020-01-13",
+            "time": "0930",
+            "amount": "800",
+            "clerk": "001",
+            "debit": "N"
+        },
+        "sender": {
+            "name": "Two Cashew",
+            "address": {
+                "street": "11110 Sunset Hills Rd",
+                "city": "Reston",
+                "state": "VA",
+                "zipCode": "20190"
+            }
+        },
+        "recipient": {
+            "name": "One Cashew",
+            "address": {
+                "street": "11110 Sunset Hills Rd",
+                "city": "Reston",
+                "state": "VA",
+                "zipCode": "20190"
+            }
+        },
+        "purchaser": {
+            "name": "null null",
+            "address": {
+                "street": null,
+                "city": null,
+                "state": null,
+                "zipCode": null
+            }
+        },
+        "image": "6bcc7483c363291052837e3c05013117",
+        "image_ref": "/6bcc7483c363291052837e3c05013117.iix",
+        "name": "Money Order #89002206237",
+        "_date_time": "2020-01-13 @ 0930",
+        "_amount": "$800",
+        "_sender": "Two Cashew",
+        "_recipient": "One Cashew",
+        "_postOffice": "RESTON",
+        "_location": "RESTON, VA"
+    }
+]
+;

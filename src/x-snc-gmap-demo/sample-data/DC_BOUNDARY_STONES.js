8cc74ea39a6ec4e8ff4327168141f1fe7a71f3a2
MarcMouries <Marc@Mouries.net>
2026-01-07 18:02:09 -0500

    new

---
+++ src/x-snc-gmap-demo/sample-data/DC_BOUNDARY_STONES.js
@@ -0,0 +1,211 @@
+/**
+ * DC Boundary Stones - Original markers of the District of Columbia
+ *
+ * The original District of Columbia was a 10-mile square marked by 40 boundary stones
+ * placed in 1791-1792 by Andrew Ellicott and Benjamin Banneker. These stones mark the
+ * corners and mile intervals along the original boundary.
+ *
+ * Data source: https://boundarystones.org/
+ */
+export const DC_BOUNDARY_STONES = [
+  {
+    name: "SOUTH",
+    description: "Jones Point Lighthouse: in an opening in the seawall of the lighthouse at Jones Point Park on the Potomac River in Alexandria, Virginia.",
+    position: { lat: 38.79033915, lng: -77.04058603 }
+  },
+  {
+    name: "SW1",
+    description: "1220 Wilkes Street: SE corner of the intersection of Wilkes and S. Payne Streets in Alexandria, Virginia.",
+    position: { lat: 38.80124355, lng: -77.05439303 }
+  },
+  {
+    name: "SW2",
+    description: "7 Russell Road: east side of Russell Road just north of King Street.",
+    position: { lat: 38.80759243, lng: -77.06263792 }
+  },
+  {
+    name: "SW3",
+    description: "2932 King Street: north end of parking lot of the First Baptist Church, south of Scroggins Road in Alexandria, Virginia.",
+    position: { lat: 38.82065264, lng: -77.0793202 }
+  },
+  {
+    name: "SW4",
+    description: "Adjacent to Fairlington Village at the edge of east side of King Street between S. Wakefield Street and Route 395.",
+    position: { lat: 38.83154299, lng: -77.09318693 }
+  },
+  {
+    name: "SW5",
+    description: "North side of Walter Reed Parkway 100+ feet east of intersection with King Street.",
+    position: { lat: 38.84208118, lng: -77.10674008 }
+  },
+  {
+    name: "SW6",
+    description: "Median strip of Jefferson Street 0.1 miles south of Columbia Pike in Arlington, Virginia.",
+    position: { lat: 38.85186534, lng: -77.11926358 }
+  },
+  {
+    name: "SW7",
+    description: "5995 5th Road, Arlington, Virginia: Carlin Springs Elementary School, parking lot C, near the fence.",
+    position: { lat: 38.86237205, lng: -77.13272594 }
+  },
+  {
+    name: "SW8",
+    description: "A short distance from the intersection of John Marshall Drive and Wilson Boulevard: 100 feet southeast of water tower behind the Patrick Henry Apartments.",
+    position: { lat: 38.87267119, lng: -77.14594527 }
+  },
+  {
+    name: "SW9",
+    description: "Benjamin Banneker Park on Van Buren Street south of 18th Street in Falls Church, Virginia.",
+    position: { lat: 38.88294291, lng: -77.15909006 }
+  },
+  {
+    name: "WEST",
+    description: "Andrew Ellicott Park: 2824 N. Arizona Street, south of West Street in Falls Church, Virginia.",
+    position: { lat: 38.89324513, lng: -77.17230114 }
+  },
+  {
+    name: "NW1",
+    description: "3607 Powhatan Street, north of 36th Street in Arlington, Virginia: west side of back yard, 200 feet from the road.",
+    position: { lat: 38.90356154, lng: -77.15914435 }
+  },
+  {
+    name: "NW2",
+    description: "5298 Old Dominion Drive or 5145 N. 38th Street, Arlington, Virginia: in the fence separating the back yards of two homes.",
+    position: { lat: 38.91388113, lng: -77.14598738 }
+  },
+  {
+    name: "NW3",
+    description: "4013 N. Tazewell Street, Arlington, Virginia: back yard of home.",
+    position: { lat: 38.92463321, lng: -77.13223642 }
+  },
+  {
+    name: "NW4",
+    description: "Within the fenced Dalecarlia Water Treatment Plant grounds, 100+ feet east of the Capitol Crescent Trail.",
+    position: { lat: 38.93760599, lng: -77.11567162 }
+  },
+  {
+    name: "NW5",
+    description: "600 feet west of Dalecarlia Parkway and 300 southeast of concrete culvert, within the fenced Dalecarlia Reservoir property.",
+    position: { lat: 38.94463908, lng: -77.10668154 }
+  },
+  {
+    name: "NW6",
+    description: "150 northeast of intersection of Park and Western Avenues in small park, across from intersection of Western Avenue and Fessenden Street.",
+    position: { lat: 38.9549086, lng: -77.09354761 }
+  },
+  {
+    name: "NW7",
+    description: "5600 Western Avenue: front yard of home near intersection with Cedar Parkway, McKinley Street, and 41st Street.",
+    position: { lat: 38.96518237, lng: -77.08040348 }
+  },
+  {
+    name: "NW8",
+    description: "6422 Western Avenue, southwest of Pinehurst Circle: front yard of home, within circular driveway.",
+    position: { lat: 38.97544235, lng: -77.06726693 }
+  },
+  {
+    name: "NW9",
+    description: "2701 Daniel Road, Chevy Chase, Maryland: 165 feet northwest of the center line of Daniel Road.",
+    position: { lat: 38.98569855, lng: -77.05412886 }
+  },
+  {
+    name: "NORTH",
+    description: "Immediately south of East-West Highway in the edge of the forest just west of the townhouses at Chevy Chase Crest condominium community.",
+    position: { lat: 38.99595651, lng: -77.04098558 }
+  },
+  {
+    name: "NE1",
+    description: "7847 Eastern Avenue, northwest of intersection with Georgia Avenue: bronze plaque in place of stone in the sidewalk in front of a shop.",
+    position: { lat: 38.98562034, lng: -77.02774336 }
+  },
+  {
+    name: "NE2",
+    description: "6980 Maple Avenue, Takoma Park, Maryland: front yard of home near intersection of Maple and Carroll Avenues.",
+    position: { lat: 38.97535127, lng: -77.01459431 }
+  },
+  {
+    name: "NE3",
+    description: "144 feet northwest of intersection of Eastern Avenue and Chillum Road at the edge of a commercial parking on the Maryland side of Eastern Avenue.",
+    position: { lat: 38.9652726, lng: -77.00169083 }
+  },
+  {
+    name: "NE4",
+    description: "5400 Sargent Road: side yard of home on Maryland side of Eastern Avenue just north of Sargent Road.",
+    position: { lat: 38.9547328, lng: -76.98820728 }
+  },
+  {
+    name: "NE5",
+    description: "4609 Eastern Avenue, northwest of Varnum Street: front yard of home.",
+    position: { lat: 38.94441385, lng: -76.97500885 }
+  },
+  {
+    name: "NE6",
+    description: "3601 Eastern Avenue, near intersection with 34th Street: front yard of home. This stone has been reset in concrete.",
+    position: { lat: 38.93371492, lng: -76.96132996 }
+  },
+  {
+    name: "NE7",
+    description: "Fort Lincoln Cemetery: along the fence in Block 18, 75 feet southwest of Garden Mausoleum near Garden of the Crucifixion.",
+    position: { lat: 38.9237882, lng: -76.94864949 }
+  },
+  {
+    name: "NE8",
+    description: "Along the fence 500 feet northwest of the intersection of Eastern and Kenilworth Avenues.",
+    position: { lat: 38.9134828, lng: -76.93548331 }
+  },
+  {
+    name: "NE9",
+    description: "919 Eastern Avenue, 0.1 miles south of Sheriff Road: front yard of home.",
+    position: { lat: 38.9031689, lng: -76.92231955 }
+  },
+  {
+    name: "EAST",
+    description: "100 feet southeast of intersection of Eastern Avenue and Southern Avenue.",
+    position: { lat: 38.8928605, lng: -76.90916553 }
+  },
+  {
+    name: "SE1",
+    description: "30 feet south of intersection of D Street and Southern Avenue, near corner of wall around National Capitol Hebrew Cemetery.",
+    position: { lat: 38.88264, lng: -76.92229346 }
+  },
+  {
+    name: "SE2",
+    description: "4345 Southern Avenue, near intersection with Rail Street: front yard of home.",
+    position: { lat: 38.87241045, lng: -76.93542433 }
+  },
+  {
+    name: "SE3",
+    description: "Across the street from 3908 Southern Avenue, near Suitland Terrace. SE3 is unusually tall for an intermediate stone.",
+    position: { lat: 38.8621606, lng: -76.94857084 }
+  },
+  {
+    name: "SE4",
+    description: "3101 Southern Avenue SE: along the gate in front of the parking lot, just south of the merger with Naylor Road.",
+    position: { lat: 38.851756, lng: -76.961934 }
+  },
+  {
+    name: "SE5",
+    description: "280 feet northeast of the northernmost intersection of Southern Avenue and Valley Terrace.",
+    position: { lat: 38.84168253, lng: -76.97483942 }
+  },
+  {
+    name: "SE6",
+    description: "901 Southern Avenue. Until early 2008, this was one of the best-looking stones.",
+    position: { lat: 38.83143717, lng: -76.9879733 }
+  },
+  {
+    name: "SE7",
+    description: "25 feet south of intersection of Southern Avenue and Indian Head Road on the east side of Indian Head Highway.",
+    position: { lat: 38.82119652, lng: -77.00109645 }
+  },
+  {
+    name: "SE8",
+    description: "Behind Blue Plains Impoundment Lot: southeast corner of the lot on the Maryland side of the fence.",
+    position: { lat: 38.81091966, lng: -77.01425046 }
+  },
+  {
+    name: "SE9",
+    description: "About 1000 feet southwest of the southern end of Oxon Cove Bridge and about 200 feet east of the Potomac River.",
+    position: { lat: 38.803728, lng: -77.023569 }
+  }
+];

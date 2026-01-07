b5e4762b96953ddcb4a8bbd6e8e5cfc1ffc22f7c
MarcMouries <Marc@Mouries.net>
2026-01-07 18:11:34 -0500

    add inline body margin reset

--- example/element.js
+++ example/element.js
@@ -1,6 +1,10 @@
 import '../src/x-snc-gmap';
 import '../src/x-snc-gmap-demo'
-import './styles.css'; 
+import './styles.css';
+
+// Remove default browser margin on body
+document.body.style.margin = '0';
+document.body.style.padding = '0';
 
 const el = document.createElement('DIV');
 el.style.width = '100%';
